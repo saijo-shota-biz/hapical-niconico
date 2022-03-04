865a377fe061a79bdefdb8b292367a38f47e5e96
さいじょー <60728259+saijo-shota-biz@users.noreply.github.com>
2022-03-05 03:36:52 +0900

    ダークモードの実装 (#50)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { FirebaseAuthProvider } from '@function/FirebaseAuthProvider';
 import { RoutesProvider } from '@function/RoutesProvider';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
 import { ConfirmModal } from '@ui/modal/ConfirmModal';
 import { Toaster } from '@ui/utils/Toaster';
+import { useMemo } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import './firebase';
 
-import { theme } from '@/theme';
+import { theme as myTheme } from '@/theme';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(() => myTheme(prefersDarkMode ? 'dark' : 'light'), [prefersDarkMode]);
+
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
